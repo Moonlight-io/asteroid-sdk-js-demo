f1ac61ad1521aa1b5162889b5a23c6feb0d41160
Travis <travis@localhost.dev>
2020-09-29 10:43:10 +1000

    Code formatting

--- examples/js/rest/user/version.js
+++ examples/js/rest/user/version.js
@@ -1,18 +1,18 @@
-const SDK = require('@moonlight-io/asteroid-sdk-js')
+const SDK = require("@moonlight-io/asteroid-sdk-js");
 
 // -- Implementation
 
-;(async () => {
-  console.log(`== Example: Get AsteroidDomainUser's version via its REST API ==`)
+(async () => {
+  console.log("== Example: AsteroidDomainUser Version ==");
 
-  const baseUrl = SDK.constants.urls.asteroidDomainUser.baseUrl.stage
-  const versionRes = await SDK.rest.user.getVersion(baseUrl)
+  const baseUrl = SDK.constants.urls.asteroidDomainUser.baseUrl.stage;
+  const versionRes = await SDK.rest.user.getVersion(baseUrl);
 
-  console.log('versionRes:', versionRes)
+  console.log("versionRes:", versionRes);
   /**
    * Example response:
    * { version: 'v1.13.19_2e9d063', uptime: 2236521, domain: 'User' }
    */
 
-  console.log('== THE END ==')
-})()
+  console.log("== THE END ==");
+})();
